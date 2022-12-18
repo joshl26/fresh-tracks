c8ca87f690982366d4125a4bc36ee96394e385aa
joshl26 <joshlehman.dev@gmail.com>
2022-12-18 13:07:56 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
 import { Route, Switch } from "react-router-dom";
 
-import { useEffect } from "react";
-
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import GoogleTrendsAPI from "./components/GoogleTrendsAPI";
-import StarWarsAPI from "./components/StarWarsAPI";
 
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    console.log("App use effect");
-  }, []);
-
   return (
     <div className="App">
       <Header />
@@ -23,8 +15,6 @@ function App() {
           <Home />
         </Route>
       </Switch>
-      <StarWarsAPI />
-      <GoogleTrendsAPI />
       <Footer />
     </div>
   );
